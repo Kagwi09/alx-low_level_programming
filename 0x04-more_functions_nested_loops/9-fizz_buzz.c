84a62892a699b9115e4047a5f160adb8575dcf2e
Kagwi09 <kagwimwangi.mk@gmail.com>
2023-08-20 12:19:19 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+*main - prints numbers 1 to 100
+*Return: 0
+*
+*/
+int main(void)
+{
+int a;
+
+for (a = 1; a <= 100; a++)
+{
+if ((a % 3 == 0) && (a % 5 == 0))
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz");
+printf(" ");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz");
+printf(" ");
+}
+else
+{
+printf("%d", a);
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
+
