2908f7135576eb9eaf4086e9a36a4f3f42057131
Kagwi09 <kagwimwangi.mk@gmail.com>
2023-08-20 11:13:23 -0700

    8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_square - prints a square
+*Return: void
+*@size: position along the x axis
+*/
+void print_square(int size)
+{
+char h;
+int m;
+int n;
+h = '#';
+
+if (size <= 0)
+{
+putchar('\n');
+}
+else
+{
+for (m = 0; m < size; m++)
+{
+for (n = 0; n < size; n++)
+{
+putchar(h);
+}
+putchar(h);
+putchar('\n');
+}
+}
+}
