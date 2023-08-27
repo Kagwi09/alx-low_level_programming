39eb3b15a125f586dcf33fec6b1705ae16332a25
Kagwi09 <kagwimwangi.mk@gmail.com>
2023-08-27 05:27:29 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,17 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int diff;
-diff = *s1 - *s2;
+int a;
+a = 0;
 
-while ((*s1 != '\0') && (*s2 != '\0'))
+while ((s1[a] != '\0') && (s2[a] != '\0'))
 {
-if (*s1 != *s2)
+if (s1[a] != s2[a])
 {
-return (diff);
+return (s1[a] - s2[a]);
 }
-s1++;
-s2++;
+a++;
 }
-return (diff);
+return (0);
 }
 
